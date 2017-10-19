a100ce05bf93be96b914092592355259cdb048a0
fengxia <feng_xia41103@hotmail.com>
2017-10-19 10:24:20 -0400

    Removed redundant jquery import. Add PDU, switch section to order detail page.

--- lxca/views.py
+++ lxca/views.py
@@ -785,6 +785,79 @@ class ArchitectApplicationDetail(DetailView):
 
 ###################################################
 #
+#	 Order solution views
+#
+###################################################
+
+
+class OrderSolutionListFilter (FilterSet):
+
+    class Meta:
+        model = OrderSolution
+        fields = ["order", "status"]
+
+
+class OrderSolutionList(FilterView):
+    template_name = "lxca/solution/order_list.html"
+    paginate_by = 10
+
+    def get_filterset_class(self):
+        return OrderSolutionListFilter
+
+
+class OrderSolutionAdd(CreateView):
+    model = OrderSolution
+    template_name = 'common/add_form.html'
+    success_url = reverse_lazy('order_solution_list')
+
+    def get_context_data(self, **kwargs):
+        context = super(CreateView, self).get_context_data(**kwargs)
+        context['title'] = u'New Order'
+        context['list_url'] = self.success_url
+        context['objects'] = OrderSolution.objects.all()
+        return context
+
+
+class OrderSolutionDelete (DeleteView):
+    model = OrderSolution
+    template_name = 'common/delete_form.html'
+    success_url = reverse_lazy('order_solution_list')
+
+    def get_context_data(self, **kwargs):
+        context = super(DeleteView, self).get_context_data(**kwargs)
+        context['title'] = u'Delete Order'
+        context['list_url'] = reverse_lazy('order_solution_list')
+        return context
+
+
+class OrderSolutionEdit(UpdateView):
+    model = OrderSolution
+    template_name = "common/edit_form.html"
+    success_url = reverse_lazy("order_solution_list")
+
+    def get_context_data(self, **kwargs):
+        context = super(UpdateView, self).get_context_data(**kwargs)
+        context['title'] = u'Edit Order'
+        context['list_url'] = reverse_lazy('order_solution_list')
+        return context
+
+
+class OrderSolutionDetail(DetailView):
+    model = OrderSolution
+    template_name = "lxca/solution/order_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailView, self).get_context_data(**kwargs)
+        context["pdu_forms"] = [
+            OrderPduForm(instance=i) for i in self.object.pdus.all()]
+        context["switch_forms"] = [
+            OrderSwitchForm(instance=i) for i in self.object.switches.all()]
+        context["server_forms"] = [
+            OrderServerForm(instance=i) for i in self.object.servers.all()]
+        return context
+
+###################################################
+#
 #	 Order PDU views
 #
 ###################################################
